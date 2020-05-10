39527fb12d064606bba7e41603b932618bc8b2c9
Karim ElGhandour <kghandour@live.com>
2020-05-10 22:52:22 +0200

    generate private key and sign message

---
+++ main.py
@@ -0,0 +1,34 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
+def generate_private_key():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    return private_key
+
+
+def sign_message(message, private_key):
+    signature = private_key.sign(
+        message.encode(),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+
+if __name__ == "__main__":
+    print("Hello world")
+    pk = generate_private_key()
+    message = "Hello world"
+    sign_message(message, pk)
+    print(sign_message)
+    
