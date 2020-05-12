fd81c84c141f168dff4154f635e55dba805df097
Karim ElGhandour <kghandour@live.com>
2020-05-12 21:28:01 +0200

    Users gen, create trans and sign, verify INCOMP

---
+++ classes/user.py
@@ -0,0 +1,34 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+class User:
+    user_id = None
+    private_key = None
+    coins = 10
+    
+    def __init__(self, userid):
+        self.private_key = self.generate_private_key()
+        self.user_id = userid
+
+    def generate_private_key(self):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
+            backend=default_backend()
+        )
+        return private_key
+    
+    def sign_message(self, message):
+        signature = self.private_key.sign(
+            message.encode(),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return signature
+    
+
--- main.py
+++ main.py
@@ -1,34 +1,56 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
+from classes.user import User
+import random
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
+users =[] 
 
-def generate_private_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    return private_key
+def initialize_users():
+    # Need to create a datastructure that stores the user
+    users = []
+    for i in range(10):
+        newUser = User(i)
+        users.append(newUser)
+    return users
 
+def create_transaction():
+    a_b = random.sample(users, 2)
+    a = a_b[0]
+    b = a_b[1]
+    amount = random.random()*a.coins
+    transaction = {}
+    transaction['sender'] = a.user_id
+    transaction['receiver'] = b.user_id
+    transaction['amount'] = amount
+    signature = a.sign_message(transaction.__str__())
+    return transaction, signature
+    
+def find_user(id):
+    for user in users:
+        if(user.user_id==id):
+            return user
+    return False
 
-def sign_message(message, private_key):
-    signature = private_key.sign(
-        message.encode(),
+def verify_signature(transaction, signature):
+    sender_id = transaction['sender']
+    sender = find_user(sender_id)
+    public_key = sender.private_key.public_key() # TODO: Getter method for public key
+    verify =  public_key.verify(
+        signature,
+        transaction.__str__().encode(),
         padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
+            mgf = padding.MGF1(hashes.SHA256()),
+            salt_length = padding.PSS.MAX_LENGTH
         ),
         hashes.SHA256()
     )
-    return signature
+    return verify
 
 
 if __name__ == "__main__":
-    print("Hello world")
-    pk = generate_private_key()
-    message = "Hello world"
-    sign_message(message, pk)
-    print(sign_message)
-    
+    users = initialize_users()
+    t, s = create_transaction()
+    print(t, s)
+    b = verify_signature(t,s)
+    print(b)
+
